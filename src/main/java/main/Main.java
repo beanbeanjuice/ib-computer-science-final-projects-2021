b60782b31e4435bc67a805d68b6f192fecf98c00
William <64740551+beanbeanjuice@users.noreply.github.com>
2021-03-10 22:01:57 -0500

    Updated Main.java and PasswordGenerator.java

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,2 +1,17 @@
+package main;
+
+import main.password.PasswordGenerator;
+
 public class Main {
+
+    private static PasswordGenerator passwordGenerator;
+
+    public static void main(String[] args) {
+
+        passwordGenerator = new PasswordGenerator(30, true);
+
+        System.out.println(passwordGenerator.generatePassword());
+
+    }
+
 }
--- src/main/java/main/password/PasswordGenerator.java
+++ src/main/java/main/password/PasswordGenerator.java
@@ -1,2 +1,33 @@
-package main.password;public class PasswordGenerator {
+package main.password;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * A password generator that can be used to generate passwords.
+ */
+public class PasswordGenerator {
+
+    int length;
+    boolean useDictionary;
+
+    /**
+     * Generate a new {@link PasswordGenerator} object.
+     * @param length The length of the passwords to be generated.
+     * @param useDictionary Choose whether or not to include dictionary words.
+     */
+    public PasswordGenerator(@NotNull Integer length, @NotNull Boolean useDictionary) {
+        this.length = length;
+        this.useDictionary = useDictionary;
+    }
+
+    /**
+     * Gets a new generated password.
+     * @return Returns a new password generated from the specified length.
+     */
+    @NotNull
+    public String generatePassword() {
+        return "test"; // Create password here
+    }
+
 }
